d34deab7f0fd87f79b957060a6762953e991c2be
Faikalgo <fathurhaikal009@gmail.com>
2023-11-09 12:06:54 +0700

    update

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import requests
 from datetime import datetime
 from bson import ObjectId
 from dotenv import load_dotenv
+import os
+from os.path import join, dirname
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
